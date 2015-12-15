4e818505f18e84eb14258f795b7003230917e319
Alex <AkJones2007@gmail.com>
2015-12-15 09:20:30 -0500

    Defines relationship between a user and their profile.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
   include Authentication
 
   has_many :gatherings
+  has_one :profile
 
 end
---
+++ db/migrate/20151215131224_add_profile_ref_to_user.rb
@@ -0,0 +1,5 @@
+class AddProfileRefToUser < ActiveRecord::Migration
+  def change
+    add_reference :users, :profile, index: true, foreign_key: true
+  end
+end
